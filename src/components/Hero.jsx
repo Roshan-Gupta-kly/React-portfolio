4e8eedb65fe25d964264c986640089072c3f0aaa
Roshan-Gupta-kly <lucysigma72@gmail.com>
2024-06-29 22:10:52 +0545

    modify with my name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
               animate="visible"
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl"
             >
-              Kevin Rush
+              Roshan Gupta
             </motion.h1>
             <motion.span
               variants={container(0.5)}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
   return (
    <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img className='mx-2 w-10' src={logo} alt="logo" />
+        {/* <img className='mx-2 w-10' src={logo} alt="logo" /> */}
+        <p className='text-4xl bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-transparent'>Roshan</p>
     </div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
       <FaLinkedin />
